97f33e9c2db55b8b6a32e09670df9e180be01e3d
i-zanis <ioanlafazanis@gmail.com>
2020-11-14 04:19:31 +0000

    dragonadded

--- src/packageFiles/Main.java
+++ src/packageFiles/Main.java
@@ -4,19 +4,27 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     // potentially here i will save the pages as a String
-    public static String logInPageFXML = "logInPage.fxml";
-    public static String mainMenuFXML = "mainMenu.fxml";
+    public static String logInPageFXML = "FXML/logInPage.fxml";
+    public static String registrationFXML = "FXML/registrationPage";
+    public static String mainMenuFXML = "FXML/mainMenu.fxml";
+    public static String productDetailFXML = "FXML/productDetail.fxml";
+    public static String basketFXML = "FXML/basket.fxml";
+    public static String checkoutFXML = "FXML/checkout.fxml";
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource(logInPageFXML));
         primaryStage.setTitle("Shenlong");
         primaryStage.setScene(new Scene(root));
+        primaryStage.getIcons().add(new Image("packageFiles/Media/dragonPic.png"));
         primaryStage.show();
     }
 
