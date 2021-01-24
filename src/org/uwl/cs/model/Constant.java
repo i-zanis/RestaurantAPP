fe16901f7a7089d6d03fde3fe3115971d9b440b8
i-zanis <55964867+i-zanis@users.noreply.github.com>
2021-01-24 02:04:40 +0000

    fixed css error path in fxml

--- src/org/uwl/cs/model/Constant.java
+++ src/org/uwl/cs/model/Constant.java
@@ -1,13 +1,13 @@
 package org.uwl.cs.model;
 
 public class Constant {
-    public static String LOGINPAGE    = "/org/uwl/cs/resources/fxml/loginpage.fxml";
-    public static String REGISTRATION = "/org/uwl/cs/resources/fxml/registrationpage.fxml";
-    public static String MAINMENU     = "/org/uwl/cs/resources/FXML/mainmenu.fxml";
-    public static String CHECKOUT     = "/org/uwl/cs/resources/FXML/checkout.fxml";
-    public static String THANKYOU     = "/org/uwl/cs/resources/FXML/thankyou.fxml";
-    public static String FAILED       = "/org/uwl/cs/resources/FXML/authenticationfailedpage.fxml";
-    public static String CSS     = "/org/uwl/cs/resources/css/style.css";
+    public static String LOGINPAGE    = "/org/uwl/cs/view/loginpage.fxml";
+    public static String REGISTRATION = "/org/uwl/cs/view/registrationpage.fxml";
+    public static String MAINMENU     = "/org/uwl/cs/view/mainmenu.fxml";
+    public static String CHECKOUT     = "/org/uwl/cs/view/checkout.fxml";
+    public static String THANKYOU     = "/org/uwl/cs/view/thankyou.fxml";
+    public static String FAILED       = "/org/uwl/cs/view/authenticationfailedpage.fxml";
+    public static String CSS           = "/org/uwl/cs/resources/css/style.css";
     public static String DRAGON_PIC    = "org/uwl/cs/resources/media/dragonpic.png";
     public final static String pound = "£";
     public final static String multi = "x";
